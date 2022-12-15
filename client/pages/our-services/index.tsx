ecd8787f63df0ff66f9b80b14ef4cefea0c8ee63
s-alad <n7.saad@gmail.com>
2022-12-14 23:07:54 -0500

    main

---
+++ client/pages/our-services/index.tsx
@@ -0,0 +1,18 @@
+import s from './services.module.css'
+
+export default function OurServices() {
+    return (
+        <div className={s.services}>
+            <div className={s.banner}>
+                Our Services
+            </div>
+            <div className={s.info}>
+                <div className={s.title}>Liquid Barrier Proudly Offers The Following Services</div>
+                <div className={s.subtitle}>We help today's entrepreneurs grow their businesses through fine writing and sound documentation</div>
+            </div>
+            <div className=''>
+
+            </div>
+        </div>
+    )
+}
