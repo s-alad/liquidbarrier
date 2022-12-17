7cd357096d54f37ed9207b9e2946a78a8dfeba62
s-alad <n7.saad@gmail.com>
2022-12-17 16:59:27 -0500

    analytics

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import { Analytics } from '@vercel/analytics/react';
 import type { AppProps } from 'next/app'
 import Layout from '../layout/layout'
 import Head from 'next/head'
@@ -19,6 +20,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <script src="https://kit.fontawesome.com/fd7a666cec.js" crossOrigin="anonymous"></script>
       </Head>
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
   )
 }
