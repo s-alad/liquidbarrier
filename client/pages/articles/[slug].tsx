ea4452934bb7f7aaf0e74af83830d7d558e54874
s-alad <n7.saad@gmail.com>
2023-01-02 22:21:29 -0500

    other articles functioanlity

--- client/pages/articles/[slug].tsx
+++ client/pages/articles/[slug].tsx
@@ -2,14 +2,16 @@ import {getArticle, getAllArticles } from '../../api/content'
 import { /* documentToHtmlString,  */documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
 import s from './articleslug.module.css'
+import Link from 'next/link';
 
 export async function getStaticPaths() {
 
-    let data = await getAllArticles();
-
+    let data: Array<any> = await getAllArticles();
     return {
         paths: data.map((article: any) => ({
-            params: { slug: article.fields.slug }
+            params: { 
+                slug: article.fields.slug
+            }
         })),
         fallback: false
     }
@@ -17,17 +19,18 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({params}:any) {
     let data = await getArticle(params.slug);
-
+    let extra = await getAllArticles();
     return {
         props: {
-            article: data[0]
+            article: data[0],
+            extra: extra.filter((article:any) => article.fields.slug !== params.slug)
         },
         revalidate: 60
     }
 }
 
-export default function Article({article}:any) {
-    console.log(article)
+export default function Article({article, extra}:any) {
+    //console.log(article)
     return (
         <div className={s.article}>
             <h1 className={s.title}>{article['fields']['title']}</h1>
@@ -35,6 +38,17 @@ export default function Article({article}:any) {
             <div className={s.date}>{article['fields']['date']}</div>
             <div className={s.content}>
                {documentToReactComponents(article['fields']['content'])}
+            </div>
+            <div className={s.seperator}></div>
+            <div className={s.otherarticles}>
+                <div className={s.title} onClick={() => console.log(extra)}>Other Articles</div>
+                <div>
+                    {extra.map((article:any) => (
+                        <Link href={article.fields.slug} key={article.sys.id} className={s.otherarticle} >
+                            <div className={s.othertitle}>{article.fields.title}</div>
+                        </Link>
+                    ))}
+                </div>
             </div>
         </div>
     )
